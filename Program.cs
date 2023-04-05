8e3e2bdc0b6427a82b6b35815701b713d0aee87c
Pseudowitchy <108087286+Pseudowitchy@users.noreply.github.com>
2023-04-05 14:34:29 -0400

    Added unit switcher to change the displayed units.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.VisualBasic.FileIO;
+using Microsoft.VisualBasic.FileIO;
+
+if (!File.Exists("settings.csv")) unitSet();
 
 while (true)
 {
     if (File.Exists("weight_data.csv") && File.Exists("weight_goal.csv"))
     {
-        Console.WriteLine($"Your starting weight was: {start():0.0} lbs. Your current goal weight is: {goal():0.0} lbs\n");
+        Console.WriteLine($"Your starting weight was: {start():0.0} {unit()}. Your current goal weight is: {goal():0.0} {unit()}\n");
         Console.WriteLine("What would you like to do? ");
         Console.WriteLine("  1 - Record Weight");
         Console.WriteLine("  2 - Report Past 10 Entries");
         Console.WriteLine("  3 - Parse Overall Change");
+        Console.WriteLine("  8 - Set/Change your displayed units");
         Console.WriteLine("  9 - Set/Change your Goal Weight");
         Console.WriteLine("  0 - Exit");
         int reply = Convert.ToInt32(Console.ReadLine());
@@ -16,6 +19,7 @@ while (true)
         if (reply == 1) write();
         else if (reply == 2) read();
         else if (reply == 3) parse();
+        else if (reply == 8) unitSet();
         else if (reply == 9) goalSet();
         else if (reply == 0) break;
         else Console.WriteLine("Bad entry, please try again.");
@@ -64,15 +68,15 @@ void parse() // Show over all weightloss up til the most recent entry, show weig
     double weightChangeDay = weightChange / duration.TotalDays;
     double weightToGoal = weightData[0].Weight - goal();
 
-    Console.WriteLine($"You have recorded your weight {count} times over {duration.TotalDays} days. You began your tracking on {weightData[0].Date:MM/dd/yyyy} at {weightData[0].Weight:0.0} lbs.\r\n");
+    Console.WriteLine($"You have recorded your weight {count} times over {duration.TotalDays} days. You began your tracking on {weightData[0].Date:MM/dd/yyyy} at {weightData[0].Weight:0.0} {unit()}.\r\n");
     if (weightChange > 0)
     {
-        Console.WriteLine($"You have lost a total of {weightChange:0.0} lbs over that period. This equates to an average {weightChangeDay * 7:0.0} lbs per week, or {weightChangeDay:0.0} lbs lost per day! :)");
-        Console.WriteLine($"Your goal weight is {goal():0.0} and you are currently {weightToGoal:0.0} lbs away from reaching it!");
+        Console.WriteLine($"You have lost a total of {weightChange:0.0} {unit()} over that period. This equates to an average {weightChangeDay * 7:0.0} {unit()} per week, or {weightChangeDay:0.0} {unit()} lost per day! :)");
+        Console.WriteLine($"Your goal weight is {goal():0.0} and you are currently {weightToGoal:0.0} {unit()} away from reaching it!");
     }
     else
     {
-        Console.WriteLine($"So far your weight has gone up by a total of {weightChange:0.0} lbs over that period. This equates to an average {weightChangeDay * 7:0.0} lbs per week, or {weightChangeDay:0.0} lbs lost per day.");
+        Console.WriteLine($"So far your weight has gone up by a total of {weightChange:0.0} {unit()} over that period. This equates to an average {weightChangeDay * 7:0.0} {unit()} per week, or {weightChangeDay:0.0} {unit()} lost per day.");
     }
 
     Console.Write("\r\nPress any key to return to the main menu.");
@@ -95,7 +99,7 @@ void read()
     {
         count++;
         WeightData historical = new(Convert.ToDouble(entries![i]), Convert.ToDateTime(entries![i - 1]));
-        Console.WriteLine($"{count:00}: {historical.Date:MM/dd/yyyy} - {historical.Weight:0.0} lbs"); 
+        Console.WriteLine($"{count:00}: {historical.Date:MM/dd/yyyy} - {historical.Weight:0.0} {unit()}"); 
     }
 
     Console.Write("\r\nPress any key to return to the main menu.");
@@ -110,7 +114,7 @@ void write()
 
     WeightData input = new(reply);
 
-    Console.WriteLine($"Your weight is {input.Weight:0.0} lbs and today's date is: {input.Date:MM/dd/yyyy}.\r\n");
+    Console.WriteLine($"Your weight is {input.Weight:0.0} {unit()} and today's date is: {input.Date:MM/dd/yyyy}.\r\n");
 
     using StreamWriter writer = new("weight_data.csv", append: true);
     writer.Write($"{input.Date:MM/dd/yyyy},{input.Weight:0.0},");
@@ -123,6 +127,16 @@ void goalSet()
 
     using StreamWriter writer = new("weight_goal.csv");
     writer.WriteLine(goal);
+}
+
+string unit() { return File.ReadAllText("settings.csv"); }
+
+void unitSet()
+{
+    Console.WriteLine("What unit type would you like to use?");
+    string? unit = Console.ReadLine();
+    using StreamWriter writer = new("settings.csv");
+    writer.Write(unit);
 }
 
 class WeightData
